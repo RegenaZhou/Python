16a690d2bf16a4e9b6260b23f9c4fd649f440cac
RegenaZhou <RegenaZhou2005@outlook.com>
2025-09-14 19:39:18 +0800

    Add files via upload

---
+++ book/Part1/Program exercise questions/1.2.py
@@ -0,0 +1,5 @@
+n=input("请输入整数N:")
+sum1=0
+for i in range(int(n)):
+    sum1+=i+1
+print("1到N之和:",sum1)
---
+++ book/Part1/Program exercise questions/1.4.py
@@ -0,0 +1,5 @@
+sum1,tmp=0,1
+for i in range(1,11):
+    tmp*=i
+    sum1+=tmp
+print("运算结果是:{}".format(sum1))
