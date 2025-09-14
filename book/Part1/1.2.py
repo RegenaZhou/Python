33d85e2204877edff157edd29ac856fa02cdfa3f
RegenaZhou <RegenaZhou2005@outlook.com>
2025-09-14 19:35:56 +0800

    Delete book/Part1 directory

--- book/Part1/1.2.py
+++
@@ -1,5 +0,0 @@
-n=input("请输入整数N:")
-sum1=0
-for i in range(int(n)):
-    sum1+=i+1
-print("1到N之和:",sum1)
--- book/Part1/1.3.py
+++
@@ -1,4 +0,0 @@
-for i in range(1,10): # 1~9
-    for j in range(1,i+1): # 1~i
-        print("{}*{}={:<2}".format(i,j,i*j),end=' ')
-    print('')
