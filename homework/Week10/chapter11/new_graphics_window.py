61c9b6d663dbe1d8ee88db73006e0de531172daf
RegenaZhou <RegenaZhou2005@outlook.com>
2026-01-01 23:51:24 +0800

    Add files via upload

---
+++ homework/Week10/chapter11/new_graphics_window.py
@@ -0,0 +1,9 @@
+# New Graphics Window
+# Demonstrates creating a graphics window
+# 新图形窗口
+# 演示创建图形窗口
+from livewires import games
+
+games.init(screen_width = 640, screen_height = 480, fps = 50)
+
+games.screen.mainloop()
